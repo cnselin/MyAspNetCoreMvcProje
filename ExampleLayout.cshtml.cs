420c0a768410004766ab4b8ece002022a20911eb
cnselin <115400670+cnselin@users.noreply.github.com>
2023-07-24 15:57:25 +0300

    Shared

---
+++ ExampleLayout.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyAspNetCoreApp.Web.Views.Shared
+{
+	public class ExampleLayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
